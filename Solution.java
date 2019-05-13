74341d9bf5b5a8153ce236bfdbfc6875d189a867
uric77 <33411049+uric77@users.noreply.github.com>
2019-05-12 17:50:05 -0700

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,22 +1,62 @@
-package com.javarush.task.task14.task1413;
+package com.javarush.task.task21.task2104;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /* 
-Computer
+Equals and HashCode
 */
+public class Solution
+{
+    private final String first, last;
 
-public class Solution {
-    public static void main(String[] args) {
-        Computer computer = new Computer(new Keyboard(), new Mouse(), new Monitor());
-        if (isWork(computer.getKeyboard()) &&
-                isWork(computer.getMonitor()) &&
-                isWork(computer.getMouse())) {
-            System.out.println("Work!");
-        }
+    public Solution(String first, String last) 
+    {
+        this.first = first;
+        this.last = last;
     }
 
-    public static boolean isWork(CompItem item) {
-        System.out.println(item.getName());
-        return item.getName() != null && item.getName().length() > 4;
+ /*  public boolean equals(Solution n) 
+    {
+         
+             if(n instanceof Solution) 
+             {
+               if((first!=null && first==n.first) && (last!=null && last==n.last))
+                return true;
+                else return false;
+             }
+                else return false;
+    } */
+    
+     @Override
+    public boolean equals(Object obj)
+    {
+        
+             if(!(obj instanceof Solution))  return false;
+        
+        Solution n = (Solution) obj;
+        if (obj == this) return true;
+        
+        boolean firstPart = (first != null) ? first==n.first : n.first == null;
+        
+        boolean secondPart = (last != null) ? last==n.last : n.last == null;
+        return firstPart && secondPart;  
+        
+        
     }
 
+    public int hashCode() 
+    {
+     int firsthashcode = (first==null)  ? 0 : first.hashCode();
+     int lasthashcode = (last == null)  ? 0 : last.hashCode();
+     
+      return 31 * firsthashcode  + lasthashcode;
+    } 
+
+    public static void main(String[] args) 
+    {
+        Set<Solution> s = new HashSet<>();
+        s.add(new Solution("Donald", "Duck"));
+        System.out.println(s.contains(new Solution("Donald", "Duck")));
+    }
 }
