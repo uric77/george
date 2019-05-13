38d373ba2132775fa75423000ef4f48ab2b7b993
uric77 <33411049+uric77@users.noreply.github.com>
2019-05-12 17:46:43 -0700

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,18 +1,22 @@
-package com.javarush.task.task11.task1101;
+package com.javarush.task.task14.task1413;
 
 /* 
-Лошадь и Пегас
+Computer
 */
 
 public class Solution {
     public static void main(String[] args) {
+        Computer computer = new Computer(new Keyboard(), new Mouse(), new Monitor());
+        if (isWork(computer.getKeyboard()) &&
+                isWork(computer.getMonitor()) &&
+                isWork(computer.getMouse())) {
+            System.out.println("Work!");
+        }
     }
 
-    public class Horse {
-
+    public static boolean isWork(CompItem item) {
+        System.out.println(item.getName());
+        return item.getName() != null && item.getName().length() > 4;
     }
 
-    public class Pegas extends Horse {
-
-    }
 }
